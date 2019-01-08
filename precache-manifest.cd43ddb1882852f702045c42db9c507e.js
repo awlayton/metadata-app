1432fce05cc8f885c2e69febc7e8481d9bee4de4
Alex Layton <alex@layton.in>
2019-01-08 16:30:33 -0500

    Updates

---
+++ precache-manifest.cd43ddb1882852f702045c42db9c507e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "211bd074136a2022dfb3",
+    "url": "/metadata-app/static/css/main.f63a7930.chunk.css"
+  },
+  {
+    "revision": "211bd074136a2022dfb3",
+    "url": "/metadata-app/static/js/main.211bd074.chunk.js"
+  },
+  {
+    "revision": "a65507c04dfcdd3683e0",
+    "url": "/metadata-app/static/css/1.f23300ff.chunk.css"
+  },
+  {
+    "revision": "a65507c04dfcdd3683e0",
+    "url": "/metadata-app/static/js/1.a65507c0.chunk.js"
+  },
+  {
+    "revision": "95cfd4694215ba3234c9",
+    "url": "/metadata-app/static/js/runtime~main.95cfd469.js"
+  },
+  {
+    "revision": "3daa3de6693b5a2aaf6c1e9d2c49817a",
+    "url": "/metadata-app/index.html"
+  }
+];
