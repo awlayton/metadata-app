c167d8aee90de8268994cb141255a40f21eda915
Alex Layton <alex@layton.in>
2019-01-24 19:27:04 -0500

    Add page for B4UFLY questions

--- src/module/index.js
+++ src/module/index.js
@@ -62,7 +62,6 @@ export default Module({
                             name: 'location',
                             title: 'Collection location',
                             type: 'panel',
-                            state: 'collapsed',
                             elements: [
                                 {
                                     name: 'location-button',
@@ -74,11 +73,13 @@ export default Module({
                                     name: 'latitude',
                                     title: 'Latitude',
                                     type: 'text',
+                                    isRequired: true,
                                 },
                                 {
                                     name: 'longitude',
                                     title: 'Longitude',
                                     type: 'text',
+                                    isRequired: true,
                                 },
                             ],
                         },
@@ -90,8 +91,8 @@ export default Module({
                     ]
                 },
                 {
-                    name: 'uav',
-                    title: 'UAV Info',
+                    name: 'uav-operator',
+                    title: 'UAV Operator',
                     visibleIf: '{purpose} == "UAV"',
                     elements: [
                         {
@@ -134,6 +135,64 @@ export default Module({
                             cerebralbutton: 'showSensorQRScanner',
                         },
                     ]
+                },
+                {
+                    name: 'b4ufly',
+                    title: 'B4UFLY Status',
+                    visibleIf: '{purpose} == "UAV"',
+                    elements: [
+                        {
+                            name: 'b4ufly-status',
+                            title: 'Status',
+                            type: 'dropdown',
+                            isRequired: true,
+                            choices: [
+                                'proceed with caution',
+                                'flying in controlled airspace (class B/C/D/E)',
+                                'other',
+                            ],
+                        },
+                        {
+                            name: 'b4ufly-extra',
+                            title: '',
+                            type: 'panel',
+                            visibleIf: '{b4ufly-status} == "flying in controlled airspace (class B/C/D/E)" or {b4ufly-status} == "other"',
+                            elements: [
+                                {
+                                    name: 'b4ufly-airport-operator',
+                                    title: 'Airport Operator Contact',
+                                    type: 'text',
+                                    requiredIf: '{b4ufly-status} == "flying in controlled airspace (class B/C/D/E)"',
+                                },
+                                {
+                                    name: 'b4ufly-control-tower',
+                                    title: 'Control Tower Contact',
+                                    type: 'text',
+                                    requiredIf: '{b4ufly-status} == "flying in controlled airspace (class B/C/D/E)"',
+                                },
+                                {
+                                    name: 'b4ufly-prior-authorization',
+                                    title: 'Prior Authorization',
+                                    type: 'text',
+                                    requiredIf: '{b4ufly-status} == "flying in controlled airspace (class B/C/D/E)"',
+                                },
+                            ],
+                        },
+                        {
+                            // TODO: What the heck even is this?
+                            name: 'b4ufly-options',
+                            title: 'Options',
+                            type: 'checkbox',
+                            isRequired: true,
+                            choices: [
+                                'checked NOTAMS',
+                                'checked flight restrictions',
+                                'checked local restrictions',
+                                'checked upcoming restrictions',
+                                'checked national parks',
+                            ],
+                        },
+                    ],
                 },
                 {
                     name: 'planting',
