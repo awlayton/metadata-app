0773d7237f72e73f6b75cdb7e8eca3daf7b41c43
Alexander William Layton <awlayton@purdue.edu>
2019-02-07 15:22:19 -0500

    Allow multiple remote PICs and VOs

--- src/module/index.js
+++ src/module/index.js
@@ -125,34 +125,59 @@ export default {
                     title: 'UAV Operator',
                     visibleIf: '{purpose} == "UAV"',
                     elements: [
-                        {
-                            name: 'certificate',
-                            title: 'Certificate Number or equivalent',
-                            type: 'text',
-                            isRequired: true,
-                            placeHolder: '1234567',
-                            validators: [
-                                {
-                                    type: 'regex',
-                                    regex: /^[0-9]{7}$/,
-                                    text: 'Invalid license number'
-                                }
-                            ]
-                        },
-                        {
-                            name: 'remote-pic',
+						{
+							name: 'remote-pics',
                             title: 'Remote Pilot in Command (PIC)',
 							description: 'remote PIC - A person who holds a remote pilot certificate with an sUAS rating and has the final authority and responsibility for the operation and safety of an sUAS operation conducted under part 107.',
-                            type: 'text',
-                            isRequired: true,
-                            placeHolder: 'Rusty Shackleford'
-                        },
+							type: 'paneldynamic',
+							panelCount: 1,
+							minPanelCount: 1,
+							panelAddText: 'Add Remote PIC',
+							panelRemoveText: 'Remove Remote PIC',
+							templateTitle: 'Remote PIC #{panelIndex}',
+							templateElements: [
+								{
+									name: 'remote-pic-name',
+									title: 'Name',
+									type: 'text',
+									isRequired: true,
+									placeHolder: 'Rusty Shackleford'
+								},
+								{
+									name: 'remote-pic-certificate',
+									title: 'Certificate Number or equivalent',
+									type: 'text',
+									isRequired: true,
+									placeHolder: '1234567',
+									validators: [
+										{
+											type: 'regex',
+											regex: /^[0-9]{7}$/,
+											text: 'Invalid license number'
+										}
+									]
+								},
+
+							],
+						},
                         {
                             name: 'observer',
                             title: 'Visual Observer (VO)',
 							description: 'VO - A person acting as a flightcrew member who assists the small UA remote PIC and the person manipulating the controls to see and avoid other air traffic or objects aloft or on the ground.',
-                            type: 'text',
-                            placeHolder: 'John Doe'
+							type: 'paneldynamic',
+							panelCount: 0,
+							panelAddText: 'Add VO',
+							panelRemoveText: 'Remove VO',
+							templateTitle: 'VO #{panelIndex}',
+							templateElements: [
+								{
+									name: 'observer-name',
+									title: 'Name',
+									type: 'text',
+									isRequired: true,
+									placeHolder: 'John Doe'
+								},
+							],
                         },
                     ]
                 },
