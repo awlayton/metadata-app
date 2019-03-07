2c1ce08bdf1246a2b1094c0b06ecffb72596be34
Alex Layton <alex@layton.in>
2019-03-06 20:03:46 -0500

    Add routes to workbox for gapi?

--- src/custom-sw.js
+++ src/custom-sw.js
@@ -1,3 +1,16 @@
 /*
  * Code to append to service worker made by CRA
  */
+
+/*global workbox */
+
+// Cache Google API stuff?
+workbox.routing.registerRoute(
+    //'https://apis.google.com/js/platform.js',
+    /https:\/\/apis\.google\.com\/.*/,
+    new workbox.strategies.NetworkFirst(),
+);
+workbox.routing.registerRoute(
+    /https:\/\/[^.]*\.googleapis\.com\/.*/,
+    new workbox.strategies.NetworkFirst(),
+);
