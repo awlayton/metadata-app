ef0770c52d6f02e82e434373adb8530976161499
Alex Layton <alex@layton.in>
2019-04-03 13:08:13 -0400

    Indicated at bottom that asterisk means required

--- src/pages.js
+++ src/pages.js
@@ -1,4 +1,10 @@
 /* eslint import/no-webpack-loader-syntax: off */
 import pages from '!import-glob!./pages-loader';
 
+// Add a footer to each page
+pages.forEach(page => page.elements.push({
+    type: 'html',
+    html: '* Indicates required field',
+}));
+
 export default pages;
