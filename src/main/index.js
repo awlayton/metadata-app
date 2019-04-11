7a1b90a73975cf5eb14e6e128f07d2672d0e8311
Alexander William Layton <awlayton@purdue.edu>
2019-04-11 16:58:10 -0400

    Use computeds to enable/disable buttons

--- src/main/index.js
+++ src/main/index.js
@@ -2,6 +2,7 @@ import localstore from './modules/localstore';
 
 import * as providers from './providers';
 import * as sequences from './sequences';
+import * as computeds from './computeds';
 import * as errors from './errors';
 
 // TODO: Should this be here??
@@ -13,6 +14,7 @@ const store = localstore(['pageNum', 'surveyData']);
 export default {
     sequences,
     state: {
+        ...computeds,
         loggedin: null,
         appdataId: null,
         resultsId: null,
