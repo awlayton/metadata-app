0a55484847e50daea9caf39ecdb4c9095f22706b
Alex Layton <alex@layton.in>
2019-05-03 12:28:08 -0400

    Add page for harvest crop conditions

---
+++ src/pages/harvest/conditions.js
@@ -0,0 +1,53 @@
+export default {
+    name: 'harvest-conditions',
+    title: 'Crop Conditions',
+    visibleIf: '{purpose} == "Harvest"',
+    elements: [
+        {
+            name: 'harvest-conditions-moisture',
+            title: 'Grain Average Moisture (%)',
+            type: 'text',
+            inputType: 'number',
+            isRequired: false,
+            placeHolder: '5 %',
+        },
+        {
+            name: 'harvest-conditions-anticipated-yield-avg',
+            title: 'Anticipated Yield Average (bu/ac)',
+            type: 'text',
+            inputType: 'number',
+            isRequired: false,
+            placeHolder: '250 bu/ac',
+        },
+        {
+            name: 'harvest-conditions-lodging',
+            title: 'Lodging',
+            type: 'dropdown',
+            isRequired: true,
+            choices: [
+                'None',
+                'Slight',
+                'Moderate',
+                'Severe',
+            ],
+        },
+        {
+            name: 'harvest-conditions-grain-quality',
+            title: 'Grain Quality',
+            type: 'tagbox',
+            select2Config: {
+                tags: true,
+                placeholder: 'Add conditions',
+                tokenSeparators: [','],
+            },
+            isRequired: false,
+            choices: [
+                'Moldy',
+                'Splits',
+                'Grain Damage',
+                'High Foreign Matter',
+                'Sprouting Seeds',
+            ],
+        },
+    ],
+};
