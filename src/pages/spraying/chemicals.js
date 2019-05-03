b778356562d7a085f00feb153a35832e9e968b35
Alex Layton <alex@layton.in>
2019-05-03 11:49:49 -0400

    Add spraying chemicals page

---
+++ src/pages/spraying/chemicals.js
@@ -0,0 +1,33 @@
+export default {
+    name: 'spraying-chemicals',
+    title: 'Chemicals',
+    visibleIf: '{purpose} == "Spraying"',
+    elements: ['Herbicide', 'Insecticide', 'Fungicide', 'Fertilizer']
+        .map(chem => ({
+            name: `spraying-chemicals-${chem.toLowerCase()}s`,
+            title: `${chem}s`,
+            type: 'paneldynamic',
+            panelCount: 0,
+            panelAddText: `Add ${chem}`,
+            panelRemoveText: `Remove ${chem}`,
+            autofill: 'lastused',
+            templateTitle: `${chem} #{panelIndex}`,
+            templateElements: [
+                {
+                    name: `spraying-checmicals-${chem.toLowerCase()}s-product`,
+                    valueName: 'product',
+                    title: 'Product Name',
+                    type: 'text',
+                    isRequired: true,
+                },
+                {
+                    name: `spraying-checmicals-${chem.toLowerCase()}s-amount`,
+                    valueName: 'amount',
+                    title: 'Amount per Acre (this product)',
+                    type: 'text',
+                    inputType: 'number',
+                    isRequired: true,
+                },
+            ],
+        })),
+};
